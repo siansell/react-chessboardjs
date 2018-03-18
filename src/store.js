2bcbe1bf0b5fa908b1d406a0cad052c3969ab635
Simon Ansell <siansell@gmail.com>
2018-03-18 10:21:17 +0000

    remove redux devtools so SSR works

--- src/store.js
+++ src/store.js
@@ -153,7 +153,7 @@ const reducer = (state = initialState, action) => {
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunk),
 )
 
